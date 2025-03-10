03b324727ee557ee8e16993ed87892752fd20a63
jhonpulido1990 <3825@holbertonstudents.com>
2025-03-10 10:33:02 -0500

    environments

--- 03-gifs-app/src/app/gifs/components/gifs-side-menu/gifd-side-menu-header/gifd-side-menu-header.component.ts
+++ 03-gifs-app/src/app/gifs/components/gifs-side-menu/gifd-side-menu-header/gifd-side-menu-header.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { environment } from '../../../../../environments/environment';
 
 @Component({
   selector: 'app-gifd-side-menu-header',
   imports: [],
   templateUrl: './gifd-side-menu-header.component.html',
 })
-export class GifdSideMenuHeaderComponent { }
+export class GifdSideMenuHeaderComponent {
+  envs = environment
+}
