5107f61cc942231c746dab5c5656ab6eb3a13c6e
jhonpulido1990 <3825@holbertonstudents.com>
2025-03-10 10:18:36 -0500

    routerlink desde componentes hijos

--- 03-gifs-app/src/app/gifs/components/gifs-side-menu/gifd-side-menu-options/gifd-side-menu-options.component.ts
+++ 03-gifs-app/src/app/gifs/components/gifs-side-menu/gifd-side-menu-options/gifd-side-menu-options.component.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOptions {
+  icon: string;
+  label: string;
+  router: string;
+  sublabel: string;
+}
 
 @Component({
   selector: 'app-gifd-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './gifd-side-menu-options.component.html',
 })
-export class GifdSideMenuOptionsComponent { }
+export class GifdSideMenuOptionsComponent {
+  menuOption: MenuOptions[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      sublabel: 'gifs populares',
+      router: '/dashboard/trending',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      sublabel: 'Buscar gifs',
+      router: '/dashboard/search',
+    },
+  ];
+}
