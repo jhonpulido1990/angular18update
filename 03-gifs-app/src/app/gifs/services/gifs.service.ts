9499fb4916dc60d6d3d2cacf8c42c6ff2b9ba16b
jhonpulido1990 <3825@holbertonstudents.com>
2025-03-12 16:17:34 -0500

    peticion HTTP GET

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,14 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import type { GiphyResponse } from '../interfaces/giphy.interfaces';
+import { Gif } from '../interfaces/gif.interface';
+import { GifMapper } from '../mapper/gif.mapper';
 
 @Injectable({ providedIn: 'root' })
 export class GifService {
   private http = inject(HttpClient);
+  public trendingGifs = signal<Gif[]>([]);
 
   constructor() {
     this.loadTrendingGifs();
@@ -17,8 +20,9 @@ export class GifService {
         api_key: environment.giphy_apiKey,
         limit: 20,
       }
-    }).subscribe((data) => {
-      console.log(data)
+    }).subscribe((resp) => {
+      const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+      this.trendingGifs.set(gifs);
     });
   }
 }
