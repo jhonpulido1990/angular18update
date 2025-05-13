48cb667a7a1bac57836e5d25290ba0ca613ea6f6
jhonpulido1990 <3825@holbertonstudents.com>
2025-05-13 17:42:10 -0500

    preservar la posicion del scroll

--- 03-gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
+++ 03-gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,15 +1,26 @@
-import { Component, ElementRef, inject, OnInit, viewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, OnInit, viewChild } from '@angular/core';
 import { GifService } from '../../services/gifs.service';
+import { ScrollStateService } from '../../shared/services/scroll-stae.service';
 
 @Component({
   selector: 'app-trending-page',
   imports: [],
   templateUrl: './trending-page.component.html',
 })
-export default class TrendingPageComponent {
+export default class TrendingPageComponent implements AfterViewInit {
   gifService = inject(GifService);
+  scrollStateService = inject(ScrollStateService);
 
   scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv');
+
+  ngAfterViewInit(): void {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if (!scrollDiv) return;
+
+    scrollDiv.scrollTop = this.scrollStateService.trendingScrollTop();
+
+    scrollDiv.addEventListener('scroll', this.onScroll.bind(this));
+  }
 
   onScroll(event: Event) {
     const scrollDiv = this.scrollDivRef()?.nativeElement
@@ -23,11 +34,13 @@ export default class TrendingPageComponent {
     // const scrollTotal = scrollTop + clientHeigth;
     const isAtBottom = scrollTop + clientHeigth + 300 >= scrollHeight;
 
-    console.log('Scroll Top:', scrollTop);
+    this.scrollStateService.trendingScrollTop.set(scrollTop);
+
+    /* console.log('Scroll Top:', scrollTop);
     console.log('Client Height:', clientHeigth);
     console.log('Scroll Height:', scrollHeight);
     // console.log('Scroll Total:', scrollTotal);
-    console.log('Is at bottom:', isAtBottom);
+    console.log('Is at bottom:', isAtBottom); */
 
     if (isAtBottom) {
       this.gifService.loadTrendingGifs();
