d272dddccf6c1e711e531fb575ccc03e1c5c4466
jhonpulido1990 <3825@holbertonstudents.com>
2025-05-13 14:34:26 -0500

    view chield

--- 03-gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
+++ 03-gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,27 +1,17 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { GifsListComponent } from '../../components/gifs-list/gifs-list.component';
+import { Component, ElementRef, inject, OnInit, viewChild } from '@angular/core';
 import { GifService } from '../../services/gifs.service';
-
-/* const imageUrls: string[] = [
-  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg',
-  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg',
-  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg',
-  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg',
-  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg',
-  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg',
-  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg',
-  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg',
-  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg',
-  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg',
-  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg',
-  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg',
-]; */
 
 @Component({
   selector: 'app-trending-page',
-  imports: [GifsListComponent],
+  imports: [],
   templateUrl: './trending-page.component.html',
 })
 export default class TrendingPageComponent {
   gifService = inject(GifService);
+
+  scrollDivRef = viewChild<ElementRef>('groupDiv');
+
+  onScroll(event: Event) {
+    const scrollDiv = this.scrollDivRef()?.nativeElement
+  }
 }
--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,10 +1,18 @@
 import { HttpClient } from '@angular/common/http';
-import { computed, inject, Injectable, signal } from '@angular/core';
+import { computed, effect, inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import type { GiphyResponse } from '../interfaces/giphy.interfaces';
 import { Gif } from '../interfaces/gif.interface';
 import { GifMapper } from '../mapper/gif.mapper';
-import { map, tap } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
+
+const GIF_KEY = 'gifs';
+
+const loadFromLocalStorage = () => {
+  const gifsFromLocalStorage = localStorage.getItem(GIF_KEY) || '{}';
+  const gifs = JSON.parse(gifsFromLocalStorage);
+  return gifs;
+};
 
 @Injectable({ providedIn: 'root' })
 export class GifService {
@@ -12,12 +20,25 @@ export class GifService {
   public trendingGifs = signal<Gif[]>([]);
   public trendingGifsLoading = signal(true);
 
-  searchHistory = signal<Record<string, Gif[]>>({});
+  searchHistory = signal<Record<string, Gif[]>>(loadFromLocalStorage());
   searchHistoryKeys = computed(() => Object.keys(this.searchHistory()));
 
   constructor() {
     this.loadTrendingGifs();
   }
+
+  saveGifsToLocalStorage = effect(() => {
+    const historyString = JSON.stringify(this.searchHistory());
+    localStorage.setItem(GIF_KEY, historyString);
+  });
+
+  trendingGifGroup = computed<Gif[][]>(() => {
+    const groups = [];
+    for (let i = 0; i < this.trendingGifs().length; i += 3) {
+      groups.push(this.trendingGifs().slice(i, i + 3));
+    }
+    return groups;
+  });
 
   loadTrendingGifs() {
     this.http
@@ -34,29 +55,34 @@ export class GifService {
       });
   }
 
-  searchGifs(query: string) {
+  searchGifs(query: string): Observable<Gif[]> {
     return this.http
       .get<GiphyResponse>(`${environment.giphy_url}/gifs/search`, {
         params: {
           api_key: environment.giphy_apiKey,
           limit: 20,
-          q: query
+          q: query,
         },
-      }).pipe(
-        map(({data}) => data),
-        map((items) =>  GifMapper.mapGiphyItemsToGifArray(items) ),
+      })
+      .pipe(
+        map(({ data }) => data),
+        map((items) => GifMapper.mapGiphyItemsToGifArray(items)),
         // historial
         tap((items) => {
           this.searchHistory.update((history) => ({
-            ...history, [query.toLowerCase()]: items,
-          }))
+            ...history,
+            [query.toLowerCase()]: items,
+          }));
         })
-      )
-      /* .subscribe((resp) => {
+      );
+    /* .subscribe((resp) => {
         const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
          this.trendingGifs.set(gifs);
         this.trendingGifsLoading.set(false);
         console.log({serach: gifs})
       }); */
+  }
+  getHistoryGifs(query: string): Gif[] {
+    return this.searchHistory()[query] ?? [];
   }
 }
